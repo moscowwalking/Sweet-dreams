55e79384820ae254f18802fda9c33114830009b3
Max Kravtsov <amck69@mail.ru>
2025-09-07 23:01:49 +0300

    49

--- server/server.js
+++ server/server.js
@@ -11,8 +11,7 @@ app.use(cors({
     'http://localhost:5500',
     'http://127.0.0.1:5500',
     'http://localhost:3000',
-    'https://moscowwalking.github.io/Sweet-dreams',
-    'https://sweet-dreams-rh2g.onrender.com'
+    'https://moscowwalking.github.io/Sweet-dreams/'
   ]
 }));
 
